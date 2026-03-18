45237c01e8fb7d1b4f000b401e88f7314a3dd7ed
Shi7ish <shirish07thore@gmail.com>
2026-03-18 09:00:37 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -1,61 +1,60 @@
-import express from 'express';
-import 'dotenv/config';
-import http from 'http';
-import cors from 'cors';
-import { connectDB } from './lib/db.js';
-import userRouter from './routes/userRoutes.js';
-import messageRouter from './routes/messageRoutes.js';
-import { Server } from 'socket.io';
+    import express from 'express';
+    import 'dotenv/config';
+    import http from 'http';
+    import cors from 'cors';
+    import { connectDB } from './lib/db.js';
+    import userRouter from './routes/userRoutes.js';
+    import messageRouter from './routes/messageRoutes.js';
+    import { Server } from 'socket.io';
 
-//Create Express app and HTTP server
-const app = express()
-const server = http.createServer(app)
+    //Create Express app and HTTP server
+    const app = express()
+    const server = http.createServer(app)
 
-// Initialize socket.io server
-export const io = new Server(server,{
-    cors: {origin : "*"}
-})
+    // Initialize socket.io server
+    export const io = new Server(server,{
+        cors: {origin : "*"}
+    })
 
-// Store online users
-export  const userSocketMap = {} // {userId : socket.id}
+    // Store online users
+    export  const userSocketMap = {} // {userId : socket.id}
 
-// Socket.io connection handler
-io.on("connection",(socket)=>{
-    const userId = socket.handshake.query.userId
-    console.log("User connected ",userId)
-    if (userId) {
-        userSocketMap[userId]  = socket.id    
-    }
+    // Socket.io connection handler
+    io.on("connection",(socket)=>{
+        const userId = socket.handshake.query.userId
+        console.log("User connected ",userId)
+        if (userId) {
+            userSocketMap[userId]  = socket.id    
+        }
 
-    // Emit online user to all connected clients
-    io.emit("getOnlineUsers", Object.keys(userSocketMap))
-
-    socket.on("disconnect",() => {
-        console.log("User Disconnected", userId)
-        delete userSocketMap[userId]
+        // Emit online user to all connected clients
         io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
+        socket.on("disconnect",() => {
+            console.log("User Disconnected", userId)
+            delete userSocketMap[userId]
+            io.emit("getOnlineUsers", Object.keys(userSocketMap))
+        })
     })
-})
 
-//Middleware Setup
-app.use(express.json({limit : '4mb'}))
-app.use(cors())
+    //Middleware Setup
+    app.use(express.json({limit : '4mb'}))
+    app.use(cors())
 
-// Route Setup
-app.use("/api/status",(req,res)=> res.send("Server is live"))
-app.use("/api/auth", userRouter)
-app.use("/api/messages", messageRouter)
+    // Route Setup
+    app.use("/api/status",(req,res)=> res.send("Server is live"))
+    app.use("/api/auth", userRouter)
+    app.use("/api/messages", messageRouter)
 
 
-// Connecting to DB
-await connectDB()
+    // Connecting to DB
+    await connectDB()
 
-if (process.env.NODE_ENV !== "production") {
-    const PORT = process.env.PORT || 5000
-    server.listen(PORT,()=>{
-        console.log("Server is live at Port : "+PORT)   
-    })
-}
+    const PORT = process.env.PORT || 5000;
 
-export default server
+    server.listen(PORT, () => {
+        console.log("Server is live at Port: " + PORT);
+    });
+
+    export default server
 
